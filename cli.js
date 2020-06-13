a704905a47c18362b08f7f6c9615738923272886
Fery Wardiyanto <ferywardiyanto@gmail.com>
2020-06-14 02:19:43 +0700

    chore: handle unhandledRejection
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- cli.js
+++ cli.js
@@ -3,6 +3,20 @@
 const { readFileSync } = require('fs')
 const { resolve } = require('path')
 
-const changelogFile = readFileSync(resolve(process.cwd(), 'CHANGELOG.md'))
+try {
+  const changelogFile = readFileSync(resolve(process.cwd(), 'CHANsGELOG.md'))
 
-require('./index')(changelogFile.toString())
+  require('./index')(changelogFile.toString())
+} catch (e) {
+  if (e.code === 'ENOENT') {
+    console.error('Error: Could not find CHANGELOG.md file')
+  }
+
+  process.stderr.write()
+  process.exit(1)
+}
+
+process.on('unhandledRejection', (reason, promise) => {
+	console.log('Unhandled Rejection at: Promise ', promise, ' reason: ', reason);
+	throw reason;
+});
