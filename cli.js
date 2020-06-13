ff818ea803ac6fac506f79af0dcd7649d758fa61
Fery Wardiyanto <ferywardiyanto@gmail.com>
2020-06-14 01:24:29 +0700

    chore: initial commit
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

---
+++ cli.js
@@ -0,0 +1,6 @@
+const { readFileSync } = require('fs')
+const { resolve } = require('path')
+
+const changelogFile = readFileSync(resolve(process.cwd(), 'CHANGELOG.md'))
+
+require('./index')(changelogFile.toString())
---
+++ index.js
@@ -0,0 +1,22 @@
+/**
+ * Read Changelog
+ *
+ * @param {String} changelog
+ */
+module.exports = async (changelog) => {
+  const changes = changelog.split('\n')
+
+  const [curr, prev] = changes.filter(line => {
+      return /^(###?).(?<version>\[?[0-9]\.[0-9]\.[0-9]\]?).\((.*)\)$/.test(line)
+  }).slice(0, 2).map(line => {
+      const index = changes.findIndex(text => text === line)
+      const match = line.match(/\((?<link>https?\:.*)\)./)
+
+      return { index, line, compare: match ? match.groups.link : null }
+  })
+
+  console.info(changes.slice(curr.index + 1, prev.index).join('\n').trim())
+  console.info('\n---\n')
+  console.info('Compare with previous version:', curr.compare)
+}
+
